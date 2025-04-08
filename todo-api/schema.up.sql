9079cbdf13ebebc7b199dc78826d804f059fe271
2025linw <2025linw@gmail.com>
2025-04-08 13:03:15 -0500

    Documentation in schema SQL scripts and changed to add up/down migration scripts

--- todo-api/schema.up.sql
+++ todo-api/schema.up.sql
@@ -1,19 +1,31 @@
--- Drop Schemas
-DROP SCHEMA IF EXISTS auth, data CASCADE;
-DROP EXTENSION IF EXISTS citext;
+/*
+This SQL script is an up migration script for Todo List Database
+*/
 
+/*
+Adds case insesitive text to database
 
--- Add citext module
+By default, most TEXT in tables are all citext.
+However, this may change to allow for granular control over
+searching
+*/
 CREATE EXTENSION citext;
 
 
--- Create schemas and add permissions
+/*
+Adds authentication schema and adjust permissions::
+    Give API user (todo_app) access to use schema and grant data manipulation functions to tables in schema by default
+*/
 CREATE SCHEMA auth;
 GRANT USAGE ON SCHEMA auth TO todo_app;
 ALTER DEFAULT PRIVILEGES
 IN SCHEMA auth
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO todo_app;
 
+/*
+Adds data schema and adjust permissions:
+    Give API user (todo_app) access to use schema and grant data manipulation functions to tables in schema by default
+*/
 CREATE SCHEMA data;
 GRANT USAGE ON SCHEMA data TO todo_app;
 ALTER DEFAULT PRIVILEGES
@@ -21,7 +33,9 @@ IN SCHEMA data
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO todo_app;
 
 
--- Tables in auth schema
+/*
+Create users tables in auth schema
+*/
 CREATE TABLE IF NOT EXISTS auth.users
 (
 	user_id uuid DEFAULT gen_random_uuid(),
@@ -36,8 +50,11 @@ CREATE TABLE IF NOT EXISTS auth.users
 	PRIMARY KEY (user_id)
 );
 
+/*
+Create areas table in data schema
 
--- Tables in data schema
+Area must be associated with an existing user
+*/
 CREATE TABLE IF NOT EXISTS data.areas
 (
 	area_id	uuid DEFAULT gen_random_uuid(),
@@ -53,6 +70,11 @@ CREATE TABLE IF NOT EXISTS data.areas
 	FOREIGN	KEY(user_id) REFERENCES auth.users(user_id)
 );
 
+/*
+Create projects table in data schema
+
+Project must be associated with an existing user
+*/
 CREATE TABLE IF NOT EXISTS data.projects
 (
 	project_id uuid DEFAULT gen_random_uuid(),
@@ -77,6 +99,11 @@ CREATE TABLE IF NOT EXISTS data.projects
 	FOREIGN	KEY(user_id) REFERENCES auth.users(user_id)
 );
 
+/*
+Create tasks table in data schema
+
+Task must be associated with an existing user
+*/
 CREATE TABLE IF NOT EXISTS data.tasks
 (
 	task_id uuid DEFAULT gen_random_uuid(),
@@ -103,6 +130,9 @@ CREATE TABLE IF NOT EXISTS data.tasks
 	FOREIGN	KEY(user_id) REFERENCES auth.users(user_id)
 );
 
+/*
+Create tags table in data schema
+*/
 CREATE TABLE IF NOT EXISTS data.tags
 (
 	tag_id uuid DEFAULT gen_random_uuid(),
@@ -119,16 +149,26 @@ CREATE TABLE IF NOT EXISTS data.tags
 	FOREIGN KEY(user_id) REFERENCES auth.users(user_id)
 );
 
+/*
+Create project-tags relation table in data schema
+
+If associated project or tag is deleted, delete the relationship
+*/
 CREATE TABLE IF NOT EXISTS data.project_tags
 (
 	project_id uuid,
 	tag_id uuid,
 
 	PRIMARY KEY(project_id, tag_id),
-	FOREIGN KEY(project_id) REFERENCES data.projects(project_id) ON DELETE CASCADE,
+	FOREIGN KEY(project_id) REFERENCES data.projects(project_id) ON DELETE CASCADE, #
 	FOREIGN KEY(tag_id) REFERENCES data.tags(tag_id) ON DELETE CASCADE
 );
 
+/*
+Create task-tags relation table in data schema
+
+If associated task or tag is deleted, delete the relationship
+*/
 CREATE TABLE IF NOT EXISTS data.task_tags
 (
 	task_id uuid,
