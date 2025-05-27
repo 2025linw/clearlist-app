48789c18fee88dc24106a4fdae44ef0542919c86
2025linw <2025linw@gmail.com>
2025-05-26 23:19:49 -0500

    updated deploy.yml to correctly catch changes in 'src' and ran cargo clippy

--- backend/src/bin/generatehash.rs
+++ backend/src/bin/generatehash.rs
@@ -1,12 +1,15 @@
 use argon2::Argon2;
-use password_hash::{rand_core::OsRng, SaltString, PasswordHasher};
+use password_hash::{PasswordHasher, SaltString, rand_core::OsRng};
 
 fn main() {
     let salt = SaltString::generate(&mut OsRng);
 
     let argon2 = Argon2::default();
 
-    let hash = argon2.hash_password("testpassword".as_bytes(), &salt).expect("unable to hash").to_string();
+    let hash = argon2
+        .hash_password("testpassword".as_bytes(), &salt)
+        .expect("unable to hash")
+        .to_string();
 
     println!("Hash: {}", hash);
 }
