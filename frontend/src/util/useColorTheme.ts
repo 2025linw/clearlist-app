2386feab73ca6047e0b232996477bebcd1bb8894
2025linw <2025linw@gmail.com>
2025-09-04 09:01:58 -0500

    update useColorTheme util

--- frontend/src/util/useColorTheme.ts
+++ frontend/src/util/useColorTheme.ts
@@ -1,7 +1,9 @@
+import { ColorSchemeName, useColorScheme } from 'react-native';
 
-import { ThemeName } from '#/types/theme';
+import { SystemColorMode } from '#/types/persisted';
+import { DarkThemes, ThemeName } from '#/types/theme';
 
-import * as persisted from '#/state';
+import * as persisted from '#/storage/async-storage';
 
 export function useColorTheme(): ThemeName {
   const theme = useThemeName();
@@ -10,10 +12,24 @@ export function useColorTheme(): ThemeName {
 }
 
 export function useThemeName(): ThemeName {
-  const colorSchme = useColorScheme();
-
+  const colorScheme = useColorScheme();
   const colorMode = persisted.get('colorMode');
-  const {colorMode, darkTheme} = useThemePrefs();
+  const darkTheme = persisted.get('darkTheme');
 
+  return getThemeName(colorScheme, colorMode, darkTheme);
+}
 
+function getThemeName(
+  colorScheme: ColorSchemeName,
+  colorMode: SystemColorMode,
+  darkTheme: DarkThemes,
+): ThemeName {
+  if (
+    (colorMode === 'system' && colorScheme === 'light') ||
+    colorMode === 'light'
+  ) {
+    return 'light';
+  } else {
+    return darkTheme;
+  }
 }
